3094034887d76d2ca9e099c021d9002a4d6010d4
Igor Machado <paulo.souza@anovasistemas.com.br>
2018-09-14 14:31:52 -0300

    method1 code cleaned

--- src/br/com/fatec/Evento.java
+++ src/br/com/fatec/Evento.java
@@ -6,41 +6,30 @@ import java.util.ArrayList;
 public class Evento extends Thread {
 	
 	public static ArrayList<Evento> historicoEventos = new ArrayList<Evento>();
-	
-	//tempo de espera pro proximo evento
 	int waitTime;
 	private int numeroEvento = 1;
 	static boolean random = false;
-//	SayHello oizao = new SayHello(this);
 	Timestamp tempoCriacao = new Timestamp(System.currentTimeMillis());
 	
 	public Evento(int waitTime, int numeroEvento) {
 		this.waitTime = waitTime;
-		this.setNumeroEvento(numeroEvento);
-		//this.historicoEventos = listaEventos;
+		this.numeroEvento=numeroEvento;
 	}
-	
 	
 	@Override
 	public void run() {
 		try {
 			Thread.sleep(waitTime*1000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
-		
-//		oizao.run();
-		
 		Evento evento;
-		//gera o 2º waitTime todos os subsequentes
 		if (random == true) {
 			int randomNextWaitTime = TestRandom.getRandomNumberInRange(1, 5);
 			evento = new Evento(randomNextWaitTime, getNumeroEvento()+1);
 		}else {
 			evento = new Evento(5, getNumeroEvento()+1);			
 		}
-		
 		historicoEventos.add(evento);
 		SayHello.imprimirEventos();
 		evento.start();
@@ -54,9 +43,5 @@ public class Evento extends Thread {
 
 	public int getNumeroEvento() {
 		return numeroEvento;
-	}
-
-	public void setNumeroEvento(int numeroEvento) {
-		this.numeroEvento = numeroEvento;
 	}
 }
--- src/br/com/fatec/SayHello.java
+++ src/br/com/fatec/SayHello.java
@@ -1,46 +1,21 @@
 package br.com.fatec;
 
-import java.security.Timestamp;
-
 public class SayHello implements Runnable {
-
-	//static pra sempre i++ toda iteração, contar nº de vezes que
-	//passou por esse metodo
-	private static int i = 1;
-	
-	private Evento ev;
-	
-	public SayHello(Evento ev) {
-		this.ev = ev;
-	}
 	
 	public SayHello() {
 	}
 
-	public void run(Timestamp timestamp) {
-		
-//		System.out.println("\nHello world " + ev.getNumeroEvento() + " " + timestamp +"\n");
-		
-	}
-
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	public void run() {	
 	}
 	
 	public static void imprimirEventos() {
-		
-		//reimprime a lista todas as vezes
-		/*
-		for (Evento temp : Evento.historicoEventos) {
+	/*
+	  	for (Evento temp : Evento.historicoEventos) { //reimprime a lista todas as vezes
 			System.out.println(temp);
 		}
-		*/
-		
-		//imprime somente o último elemento
+	*/
 		Evento ultimoElemento = Evento.historicoEventos.get(Evento.historicoEventos.size() - 1);
 		System.out.println(ultimoElemento);
 	}
-	
 }
